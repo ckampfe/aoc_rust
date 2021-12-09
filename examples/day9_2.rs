4085db8351d1ac6237d865cd5cb427e8428f6ff6
Clark Kampfe <clark.kampfe@gmail.com>
2021-12-09 01:27:30 -0600

    day9_2: use adjacents array for ~constant memory use

--- examples/day9_2.rs
+++ examples/day9_2.rs
@@ -4,6 +4,7 @@ const WIDTH: usize = 100;
 const HEIGHT: usize = 100;
 const MAX_WIDTH: usize = WIDTH - 1;
 const MAX_HEIGHT: usize = HEIGHT - 1;
+const MAX_INDEX: usize = WIDTH * HEIGHT - 1;
 
 struct HeightMap(Vec<u8>);
 
@@ -13,56 +14,67 @@ impl HeightMap {
     }
 }
 
-fn find_adjacents(i: usize) -> Vec<usize> {
+fn find_adjacents(i: usize, a: &mut [usize; 4]) {
+    *a = [usize::MAX; 4];
+
     let (x, y) = i_to_xy(i);
 
-    let out = match (x, y) {
+    match (x, y) {
+        // regular case
         (some_x, some_y)
             if some_x > 0 && some_x < MAX_WIDTH && some_y > 0 && some_y < MAX_HEIGHT =>
         {
-            vec![
-                xy_to_i(some_x + 1, y),
-                xy_to_i(some_x - 1, y),
-                xy_to_i(some_x, y + 1),
-                xy_to_i(some_x, y - 1),
-            ]
+            a[0] = xy_to_i(some_x + 1, y);
+            a[1] = xy_to_i(some_x - 1, y);
+            a[2] = xy_to_i(some_x, y + 1);
+            a[3] = xy_to_i(some_x, y - 1);
         }
-        (0, 0) => vec![xy_to_i(1, 0), xy_to_i(0, 1)],
-        (0, MAX_HEIGHT) => vec![xy_to_i(1, MAX_HEIGHT), xy_to_i(0, MAX_HEIGHT - 1)],
-        (MAX_WIDTH, 0) => vec![xy_to_i(MAX_WIDTH - 1, 0), xy_to_i(MAX_WIDTH, 1)],
-        (MAX_WIDTH, MAX_HEIGHT) => vec![
-            xy_to_i(MAX_WIDTH - 1, MAX_HEIGHT),
-            xy_to_i(MAX_WIDTH, MAX_HEIGHT - 1),
-        ],
-        (MAX_WIDTH, some_y) => vec![
-            xy_to_i(MAX_WIDTH, some_y + 1),
-            xy_to_i(MAX_WIDTH, some_y - 1),
-            xy_to_i(MAX_WIDTH - 1, some_y),
-        ],
-        (other_x, MAX_HEIGHT) => vec![
-            xy_to_i(other_x + 1, MAX_HEIGHT),
-            xy_to_i(other_x - 1, MAX_HEIGHT),
-            xy_to_i(other_x, MAX_HEIGHT - 1),
-        ],
-        (0, some_y) => vec![
-            xy_to_i(1, some_y),
-            xy_to_i(0, some_y - 1),
-            xy_to_i(0, some_y + 1),
-        ],
-        (some_x, 0) => vec![
-            xy_to_i(some_x + 1, 0),
-            xy_to_i(some_x - 1, 0),
-            xy_to_i(some_x, 1),
-        ],
-        _ => panic!(),
+        // upper left
+        (0, 0) => {
+            a[0] = xy_to_i(1, 0);
+            a[1] = xy_to_i(0, 1);
+        }
+        // lower left
+        (0, MAX_HEIGHT) => {
+            a[0] = xy_to_i(1, MAX_HEIGHT);
+            a[1] = xy_to_i(0, MAX_HEIGHT - 1);
+        }
+        // upper right
+        (MAX_WIDTH, 0) => {
+            a[0] = xy_to_i(MAX_WIDTH - 1, 0);
+            a[1] = xy_to_i(MAX_WIDTH, 1);
+        }
+        // lower right
+        (MAX_WIDTH, MAX_HEIGHT) => {
+            a[0] = xy_to_i(MAX_WIDTH - 1, MAX_HEIGHT);
+            a[1] = xy_to_i(MAX_WIDTH, MAX_HEIGHT - 1);
+        }
+        // right column
+        (MAX_WIDTH, some_y) => {
+            a[0] = xy_to_i(MAX_WIDTH, some_y + 1);
+            a[1] = xy_to_i(MAX_WIDTH, some_y - 1);
+            a[2] = xy_to_i(MAX_WIDTH - 1, some_y);
+        }
+        // bottom row
+        (other_x, MAX_HEIGHT) => {
+            a[0] = xy_to_i(other_x + 1, MAX_HEIGHT);
+            a[1] = xy_to_i(other_x - 1, MAX_HEIGHT);
+            a[2] = xy_to_i(other_x, MAX_HEIGHT - 1);
+        }
+        // left column
+        (0, some_y) => {
+            a[0] = xy_to_i(1, some_y);
+            a[1] = xy_to_i(0, some_y - 1);
+            a[2] = xy_to_i(0, some_y + 1);
+        }
+        // top row
+        (some_x, 0) => {
+            a[0] = xy_to_i(some_x + 1, 0);
+            a[1] = xy_to_i(some_x - 1, 0);
+            a[2] = xy_to_i(some_x, 1);
+        }
+        _ => panic!("unhandled index case"),
     };
-
-    for out_i in &out {
-        let (x, y) = i_to_xy(*out_i);
-        assert!(*out_i == xy_to_i(x, y));
-    }
-
-    out
 }
 
 const fn i_to_xy(i: usize) -> (usize, usize) {
@@ -87,21 +99,19 @@ fn is_lowpoint(value: u8, adjacent_values: &[u8]) -> bool {
         .all(|adjacent_value| value < *adjacent_value)
 }
 
-fn find_basin_indexes(i: usize, heighmap: &HeightMap) -> Vec<usize> {
+fn find_basin_indexes(i: usize, heightmap: &HeightMap, adjacents: &mut [usize; 4]) -> Vec<usize> {
     let mut basin = vec![];
     let mut queue = VecDeque::new();
     queue.push_back(i);
     basin.push(i);
 
     while let Some(this_i) = queue.pop_front() {
-        let adjacents = find_adjacents(this_i)
-            .into_iter()
-            .filter(|adj_i| heighmap.0[*adj_i] < 9);
+        find_adjacents(this_i, adjacents);
 
-        for adjacent in adjacents {
-            if !basin.contains(&adjacent) {
-                basin.push(adjacent);
-                queue.push_back(adjacent);
+        for adjacent in adjacents.iter() {
+            if *adjacent <= MAX_INDEX && !basin.contains(adjacent) && heightmap.0[*adjacent] < 9 {
+                basin.push(*adjacent);
+                queue.push_back(*adjacent);
             }
         }
     }
@@ -128,18 +138,19 @@ fn main() {
 
     let mut basin_lens = vec![];
 
-    for (i, value) in heightmap.0.iter().enumerate() {
-        let adjacents = find_adjacents(i);
+    let mut adjacents = [usize::MAX; 4];
 
-        assert!(adjacents.len() == 2 || adjacents.len() == 3 || adjacents.len() == 4);
+    for (i, value) in heightmap.0.iter().enumerate() {
+        find_adjacents(i, &mut adjacents);
 
         let adjacent_values: Vec<u8> = adjacents
             .into_iter()
+            .filter(|adjacent_i| *adjacent_i <= MAX_INDEX)
             .map(|adjacent_i| heightmap.0[adjacent_i])
             .collect();
 
         if is_lowpoint(*value, &adjacent_values) {
-            let basin_values = find_basin_indexes(i, &heightmap);
+            let basin_values = find_basin_indexes(i, &heightmap, &mut adjacents);
             let basin_len = basin_values.len();
             basin_lens.push(basin_len);
         }
